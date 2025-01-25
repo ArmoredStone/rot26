960260134e9f6358450351cbe5bb80d46272f431
Alex Aheyev <aheyevalex@gmail.com>
2025-01-24 16:39:49 -0800

    Haskell version, rotates symbols around symbol table not alphabet

--- haskell/rot26.hs
+++ haskell/rot26.hs
@@ -2,12 +2,28 @@
 -- by ArmoredStone
 -- 24.01.2025
 
+import System.IO
 import System.Environment (getArgs)
-
--- encode :: [Char] -> [Char]
--- decode :: [Char] -> [Char]
 
 main :: IO ()
 main = do
+    hSetEncoding stdout utf8
+    hSetEncoding stdin utf8
+    
     args <- getArgs
-    putStrLn $ show args
+    let mode = head (head args)
+        textChars = unwords (tail args)
+        result = modeCase mode textChars
+    putStrLn (result)
+
+modeCase :: Char -> [Char] -> [Char]
+modeCase mode text = case mode of
+    'e' -> map applySucc26times text
+    'd' -> map applyPred26times text
+    _ -> "Invalid mode, use e or d"
+
+applySucc26times :: Char -> Char
+applySucc26times = (!! 26) . iterate succ
+
+applyPred26times :: Char -> Char
+applyPred26times = (!! 26) . iterate pred
